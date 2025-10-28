5427c1407db61e0dfe7d5efa492fc20184a54669
Gus-IA <garciagus1992@gmail.com>
2025-10-28 10:25:07 +0100

    gans entrenamiento

--- gans_introduccion.py
+++ gans_introduccion.py
@@ -4,6 +4,8 @@ import numpy as np
 import random 
 import matplotlib.pyplot as plt
 import torch.nn as nn
+from fastprogress import master_bar, progress_bar
+import pandas as pd 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -92,3 +94,79 @@ plt.show()
 discriminator = MLP(28*28, 1)
 output = discriminator(torch.randn(64, 28*28))
 print(output.shape)
+
+
+
+def fit(g, d, dataloader, epochs=30, crit=None):
+  g.to(device)
+  d.to(device)
+  g_optimizer = torch.optim.Adam(g.parameters(), lr=3e-4)
+  d_optimizer = torch.optim.Adam(d.parameters(), lr=3e-4)
+  crit = nn.BCEWithLogitsLoss() if crit == None else crit
+  g_loss, d_loss = [], []
+  mb = master_bar(range(1, epochs+1))
+  hist = {'g_loss': [], 'd_loss': []}
+  for epoch in mb:
+    for X, y in progress_bar(dataloader, parent=mb):
+      #X, y = X.to(device), y.to(device)  
+      # entrenamos el discriminador
+      g.eval()
+      d.train()
+      #   generamos un batch de imágenes falsas
+      noise = torch.randn((X.size(0), g.input_size)).to(device)
+      genenerated_images = g(noise)
+      #   input del discrminator
+      d_input = torch.cat([genenerated_images, X.view(X.size(0), -1)])
+      #   gorund truth para el discriminator
+      d_gt = torch.cat([torch.zeros(X.size(0)), torch.ones(X.size(0))]).view(-1,1).to(device)
+      #   optimización
+      d_optimizer.zero_grad()
+      d_output = d(d_input)
+      d_l = crit(d_output, d_gt)
+      d_l.backward()
+      d_optimizer.step()
+      d_loss.append(d_l.item())
+      # entrenamos el generador
+      g.train()
+      d.eval()
+      #   generamos un batch de imágenes falsas
+      noise = torch.randn((X.size(0), g.input_size)).to(device)
+      genenerated_images = g(noise)
+      #   salidas del discriminador
+      d_output = d(genenerated_images)
+      #   gorund truth para el generator
+      g_gt = torch.ones(X.size(0)).view(-1,1).to(device)
+      #   optimización
+      g_optimizer.zero_grad()
+      g_l = crit(d_output, g_gt)
+      g_l.backward()
+      g_optimizer.step()
+      g_loss.append(g_l.item())
+      # logs
+      mb.child.comment = f'g_loss {np.mean(g_loss):.5f} d_loss {np.mean(d_loss):.5f}'
+    mb.write(f'Epoch {epoch}/{epochs} g_loss {np.mean(g_loss):.5f} d_loss {np.mean(d_loss):.5f}')
+    hist['g_loss'].append(np.mean(g_loss))    
+    hist['d_loss'].append(np.mean(d_loss))
+  return hist
+
+
+hist = fit(generator, discriminator, dataloader)
+
+
+df = pd.DataFrame(hist)
+df.plot(grid=True)
+plt.show()
+
+
+generator.eval()
+with torch.no_grad():
+  noise = torch.randn((10, generator.input_size)).to(device)
+  generated_images = generator(noise)
+  fig, axs = plt.subplots(2,5,figsize=(15,5))
+  i = 0
+  for ax in axs:
+    for _ax in ax:
+      img = generated_images[i].view(28,28).cpu()
+      _ax.imshow(img)
+      i+=1
+  plt.show()
